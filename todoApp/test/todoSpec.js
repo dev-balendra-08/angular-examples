6c3bf0e8fe8b89b80ae7c913a4b9eb8950d79455
Thanos Korakas <thanoskorakas@gmail.com>
2016-03-05 15:06:54 +0200

    Controller test.

--- todoApp/test/todoSpec.js
+++ todoApp/test/todoSpec.js
@@ -2,12 +2,14 @@ describe('TodoApp', function () {
   beforeEach(module('todoApp'));
 
   var todoService
+  var scope
 
   beforeEach(inject(function (_todoService_) {
     todoService = _todoService_;
   }));
 
-  describe('TodoService get.', function () {
+  describe('TodoService.', function () {
+
     it('should return 3 tasks', function () {
       var tasks = todoService.get()
       expect(tasks.length).toBe(3)
@@ -31,6 +33,35 @@ describe('TodoApp', function () {
     it('should clear all tasks', function () {
       var tasks = todoService.clear()
       expect(tasks.length).toBe(0)
+    })
+
+  })
+
+  describe('Todo controller', function () {
+    var scope
+    var ctrl
+
+    // Inject scope and controller before each test.
+    beforeEach(inject(function ($rootScope, $controller) {
+      scope = $rootScope.$new();
+      ctrl = $controller('todoCtrl', {$scope: scope});
+    }))
+
+    it('should remove 1 task', function () {
+      scope.remove(0)
+      expect(scope.tasks.length).toBe(2)
+    })
+
+    it('should add 1 task', function () {
+      scope.name = "test task"
+      scope.add()
+      expect(scope.tasks.length).toBe(4)
+      expect(scope.tasks[3].name).toBe("test task")
+    })
+
+    it('should clear all tasks', function () {
+      scope.clear()
+      expect(scope.tasks.length).toBe(0)
     })
 
   })
